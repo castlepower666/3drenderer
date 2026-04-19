b1dc1eae5583cadac9a3d7a42b7d2f194a053cc1
CastlePower <1440152984@qq.com~>
2026-04-19 22:52:49 +0800

    框架搭建

---
+++ 3drenderer/Main.c
@@ -0,0 +1,109 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <SDL.h>   
+
+bool is_running = false;
+SDL_Window* window = NULL;
+SDL_Renderer* renderer = NULL;
+uint32_t* color_buffer = NULL;
+int window_width = 800;
+int window_height = 600;
+
+bool initialize_window(void)
+{
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
+    {
+        fprintf(stderr, "SDL_Init Error\n");
+
+        return false;
+    }
+
+    window = SDL_CreateWindow(
+        NULL, 
+        SDL_WINDOWPOS_CENTERED, 
+        SDL_WINDOWPOS_CENTERED, 
+        800, 
+        600, 
+        SDL_WINDOW_BORDERLESS);
+
+    if (!window)
+    {
+        fprintf(stderr, "SDL_CreateWindow Error\n");
+
+        return false;
+    }
+
+    renderer = SDL_CreateRenderer(window, -1, 0);
+
+    if (!renderer)
+    {
+        fprintf(stderr, "SDL_CreateRenderer Error\n");
+        return false;
+    }
+
+    return true;
+}
+
+void setup(void)
+{
+    color_buffer = (uint32_t*)malloc(sizeof(uint32_t) * window_width * window_height);
+}
+
+void process_input(void)
+{
+    SDL_Event event;
+    SDL_PollEvent(&event);
+
+    switch (event.type)
+    {
+        case SDL_QUIT:
+            is_running = false;
+            break;
+        case SDL_KEYDOWN:
+            if (event.key.keysym.sym == SDLK_ESCAPE)
+            {
+                is_running = false;
+            }
+            break;
+    }
+
+
+}
+
+void update(void)
+{
+
+}
+
+void render(void)
+{
+    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+    SDL_RenderClear(renderer);
+
+    SDL_RenderPresent(renderer);
+}
+
+void destroy_window(void)
+{
+    free(color_buffer);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+}
+
+int main(int argc, char* argv[])
+{
+    is_running = initialize_window();
+
+    setup();
+
+    while (is_running)
+    {
+        process_input();
+        update();
+        render();
+    }
+
+
+    return 0;
+}
