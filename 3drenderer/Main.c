f1eba5ee804727443103a45764581a889209277f
CastlePower <1440152984@qq.com~>
2026-05-13 15:48:38 +0800

    使用动态数组重构；画线DDA算法；

--- 3drenderer/Main.c
+++ 3drenderer/Main.c
@@ -1,6 +1,10 @@
 #include "display.h"
 #include "vector.h"
 #include "mesh.h"
+#include "array.h"
+
+
+triangle_t* triangles_to_render = NULL;
 
 float fov_factor = 1024;
 vec3_t camera_position = { 0,0,-5 };
@@ -21,8 +25,7 @@ void setup(void)
         window_height
     );
 
-
-
+    load_cube_mesh_data();
 }
 
 void process_input(void)
@@ -68,20 +71,27 @@ void update(void)
         SDL_Delay(time_to_wait);
     }
 
+
     previous_frame_time = SDL_GetTicks();
 
     cube_rotation.x += 0.001;
     cube_rotation.y += 0.001;
     cube_rotation.z += 0.001;
 
-    for (int i = 0; i < N_MESH_VERTICES; i++)
+    triangles_to_render = NULL;
+
+    //遍历每个面，计算每个面的顶点在屏幕上的位置，并存储到triangles_to_render数组中
+    int num_faces = array_length(mesh.faces);
+    for (int i = 0; i < num_faces; i++)
     {
-        face_t mesh_face = mesh_faces[i];
+        face_t mesh_face = mesh.faces[i];
 
         vec3_t face_vertices[3];
-        face_vertices[0] = mesh_vertices[mesh_face.a - 1];
-        face_vertices[1] = mesh_vertices[mesh_face.b - 1];
-        face_vertices[2] = mesh_vertices[mesh_face.c - 1];
+        face_vertices[0] = mesh.vertices[mesh_face.a - 1];
+        face_vertices[1] = mesh.vertices[mesh_face.b - 1];
+        face_vertices[2] = mesh.vertices[mesh_face.c - 1];
+
+        triangle_t projected_triangle;
 
         for (int j = 0; j < 3; j++)
         {
@@ -91,8 +101,17 @@ void update(void)
             transformed_vertex = vec3_rotate_y(transformed_vertex, cube_rotation.y);
             transformed_vertex = vec3_rotate_z(transformed_vertex, cube_rotation.z);
 
+            transformed_vertex.z -= camera_position.z;
+
             vec2_t projected_vertex = project(transformed_vertex);
+
+            projected_vertex.x += window_width / 2;
+            projected_vertex.y += window_height / 2;
+
+            projected_triangle.points[j] = projected_vertex;
         }
+
+        array_push(triangles_to_render, projected_triangle);
 
     }
 
@@ -103,17 +122,25 @@ void render(void)
 {
     draw_grid();
 
-    //for (int i = 0; i < N_POINTS; i++)
-    //{
-    //    draw_rect(
-    //        projected_points[i].x + (window_width / 2),//把cube放到屏幕中心
-    //        projected_points[i].y + (window_height / 2),
-    //        4,
-    //        4,
-    //        0xFFFFFF00
-    //    );
-    //}
+    int num_triangles = array_length(triangles_to_render);
 
+    for (int i = 0; i < num_triangles; i++)
+    {
+        triangle_t triangle = triangles_to_render[i];
+
+        draw_triangle(
+            triangle.points[0].x,
+            triangle.points[0].y,
+            triangle.points[1].x,
+            triangle.points[1].y,
+            triangle.points[2].x,
+            triangle.points[2].y,
+            0xFFFFFF00
+        );
+
+    }
+
+    array_free(triangles_to_render);
 
     render_color_buffer();//颜色缓冲区渲染到渲染器上
     clear_color_buffer(0xFF000000);
@@ -121,13 +148,13 @@ void render(void)
     SDL_RenderPresent(renderer);
 }
 
-void destroy_window(void)
+void free_resources(void)
 {
     free(color_buffer);
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
+    array_free(mesh.faces);
+    array_free(mesh.vertices);
 }
+
 
 int main(int argc, char* argv[])
 {
@@ -141,6 +168,10 @@ int main(int argc, char* argv[])
         update();
         render();
     }
+
+    destroy_window();
+    free_resources();
+
 
     return 0;
 }
